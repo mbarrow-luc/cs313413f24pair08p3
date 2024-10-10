093fa41c622020f7ab8e0ee1a77e1a53a3f09f1c
mbarrow-luc <mbarrow@luc.edu>
2024-10-10 15:07:05 -0500

    Implement Draw.onRectangle

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,14 +17,16 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = canvas; // FIXME - MAYBE DONE
-        this.paint = paint; // FIXME - MAYBE DONE
+        this.canvas = canvas;
+        this.paint = paint;
         paint.setStyle(Style.STROKE);
     }
 
     @Override
     public Void onCircle(final Circle c) {
+
         canvas.drawCircle(0, 0, c.getRadius(), paint);
+
         return null;
     }
 
@@ -49,11 +51,19 @@ public class Draw implements Visitor<Void> {
     @Override
     public Void onLocation(final Location l) {
 
+        var x = (float) l.getX();
+        var y = (float) l.getY();
+        canvas.translate(x, y);
+        l.getShape().accept(this);
+        canvas.translate(-x, -y);
+
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
+
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
 
         return null;
     }
